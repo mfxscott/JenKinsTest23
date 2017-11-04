70896263df1969daf28864750ebbf7028d891a37
mfx <mfx19900211@gmail.com>
2017-11-04 17:25:39 +0800

    Initial commit

---
+++ app/src/main/java/com/example/mfx_t224/jenkinstest23/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.mfx_t224.jenkinstest23;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
